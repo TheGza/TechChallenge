4940e7ace73d6fc05ed8ff36d2ca556176e27ef0
TheGza <johncharleskelley@gmail.com>
2016-05-19 14:03:46 -0400

    Used Wait function instead of sleep in a few instances

--- TechChallenge.js
+++ TechChallenge.js
@@ -2,6 +2,8 @@ function main(){
 
 //define variables
 var webdriver = require('selenium-webdriver');
+	By = webdriver.By,
+	until = webdriver.until;
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 var fn = 'John';
 var ln = 'Kelley';
@@ -13,18 +15,17 @@ var searchString = 'Great Britain 1841 SG8 PL.127 Mint';
 //access stanley gibbons website
 driver.get('https://www.stanleygibbons.com');
 
-//sleep for 2 seconds. Used several times because webdriver runs too quickly for a physical window
-driver.sleep(2000);
-
 //every time the page is opened via the webdriver a popup is displayed asking the user to specify a homepage. This selects the Market Place
+driver.wait(until.elementLocated(By.xpath('/html/body/div[7]/div/div/div/div/div[4]/div[2]/a/div')), 10000, 'Could not locate element within time specifided');
 driver.findElement(webdriver.By.xpath('/html/body/div[7]/div/div/div/div/div[4]/div[2]/a/div')).click();
-driver.sleep(2000);
 
 //begin the process of registering a user
-driver.findElement(webdriver.By.xpath('//*[@id="header-top-icons"]/ul/li[6]/a')).click();
+//waiting won't work here because the element will always be visible, even when the popup is in front of it
 driver.sleep(2000);
+driver.findElement(webdriver.By.xpath('//*[@id="header-top-icons"]/ul/li[6]/a')).click();
 
 //fill in form with stored variables then submit the registration
+driver.wait(until.elementLocated(By.id('sg_user_registration_firstName')), 10000, 'Could not locate element within time specifided');
 driver.findElement(webdriver.By.id('sg_user_registration_firstName')).sendKeys(fn);
 driver.findElement(webdriver.By.id('sg_user_registration_lastName')).sendKeys(ln);
 driver.findElement(webdriver.By.id('sg_user_registration_email')).sendKeys(email);
@@ -40,6 +41,7 @@ driver.get('https://www.stanleygibbons.com');
 //search string is entered and search button is clicked
 driver.findElement(webdriver.By.id('main-search')).sendKeys(searchString);
 driver.findElement(webdriver.By.id('header-search-icon')).click();
+
 }
 
 main();
